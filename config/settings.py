52fa99e5a71adc9b9fe1b407d31fe3413e55d0a7
LiantsoaHarimisa <liantsoa3002@gmail.com>
2026-04-09 22:09:58 +0300

    Change config

--- config/settings.py
+++ config/settings.py
@@ -54,12 +54,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default':{
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'malagasyTextEditor',
-        'USER':'postgres',
-        'PASSWORD':'Dovahkiin150',
-        'HOST': 'localhost',
-        'PORT' : 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
